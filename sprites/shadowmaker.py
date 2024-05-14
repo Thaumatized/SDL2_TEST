a7a2b5b1881e9df87e17ebe2a7e7a2ee9a71ebe0
Thaumatized <veeti.junkkala@gmail.com>
2024-05-14 22:49:08 +0300

    Better shadow generator - This is tempoary, I think, but couldn't live with the diamond shaped shadows.

--- sprites/shadowmaker.py
+++ sprites/shadowmaker.py
@@ -1,16 +1,21 @@
 from PIL import Image
+import math
 
 output_filename = "bin/sprites/shadow.png"
 
-image = Image.new("RGBA", (64, 32))
+size_x = 64
+size_y = 32
 
-for y in range(64):
-    for x in range(32):
-        darkness = int(((0.5 - abs(y-32)/64) + (0.5 - abs(x-16)/32)) * 255)
+image = Image.new("RGBA", (size_x, size_y))
+
+for x in range(size_x):
+    for y in range(size_y):
+        distanceFromCenter = math.sqrt(pow(abs(((size_x/2)-x)/(size_x/2)), 2) + pow(abs(((size_y/2)-y)/(size_y/2)), 2))
+        darkness = 255 - int(distanceFromCenter*255)
 
         if darkness < 50:
             darkness = 0
 
-        image.putpixel((y,x), (0, 0, 0, darkness))
+        image.putpixel((x, y), (0, 0, 0, darkness))
 
 image.save(output_filename)
